909e67af90fd68024e380ebb284348b7d4e24d68
fredcaps <93043183+fredcaps@users.noreply.github.com>
2024-10-16 08:48:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import os
 from tools import collect_subdomains
 from results import filter_and_write_results
 from utils import print_message
-from database import get_next_scan_id, insert_scan_result
 
 def main():
     parser = argparse.ArgumentParser(description="Script de reconnaissance en mode passif")
@@ -33,19 +32,10 @@ def main():
     start_time = time.time()
     output_file = f"Scan_du_{datetime.now().strftime('%Y-%m-%d_%H-%M')}_results.txt"
 
-    # Obtenir le prochain scan_id
-    scan_id = get_next_scan_id()
-
     for domain in domains:
         subdomains = collect_subdomains(domain, tools)
-        results = filter_and_write_results(subdomains, domain, output_file)
-
-        # Insérer les résultats dans la base de données
-        for result in results:
-            # result est un tuple : (domain, subdomain, http_codes, ports, ip_address, scan_time)
-            insert_scan_result(scan_id, *result)
-
-        print(f"Résultats pour {domain} enregistrés dans la base de données")
+        filter_and_write_results(subdomains, domain, output_file)
+        print(f"Résultats pour {domain} enregistrés dans {output_file}")
 
     duration = time.time() - start_time
     print_message(f"Le script s'est terminé en {duration:.2f} secondes.", symbol="#")
